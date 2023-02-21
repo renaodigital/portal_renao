7799c16646c3b4f6cde56c0173cab561f285a210
renaodigital <renaodigitalcc@gmail.com>
2023-02-21 01:45:13 -0300

    21/02/2023 [01h44min]
    1st Stage completed.
    2nd & 3rd (soon){
    }

--- website (html-css-js)/home_page/app.js
+++ website (html-css-js)/home_page/app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo')
 
 // Display Mobile Menu
 const mobileMenu = () => {
@@ -7,4 +8,46 @@ const mobileMenu = () => {
     menuLinks.classList.toggle('active');
 }
 
-    menu.addEventListener('click', mobileMenu);
+menu.addEventListener('click', mobileMenu);
+
+/ / Show active menu when scrolling 
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight')
+    const homeMenu = document.querySelector('#home-page')
+    const aboutMenu = document.querySelector('#about-page')
+    const whatimdoingnowMenu = document.querySelector('#portfolio-page')
+    let scrollPos = window.scrollY
+    console.log(scrollPos);
+
+    / / adds 'highlight' class to my menu items
+    if(window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+        whatimdoingnowMenu.classList.remove('highlight')
+        aboutMenu.classList.add('highlight')
+        return
+    }   else if (window.inneWidth > 960 && scrollPos < 1400) {
+        aboutMenu.classList.add
+    }
+
+    if ((elem && window.innerWidth < 960 && scrollPos < 600 ) || elem) {
+        elem.classList.remove('highlight')
+    }
+}
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('scroll', highlightMenu);
+
+/ / Close mobile Menu when clicking on a menu Item 
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active')
+    if(window.innerWidth <== 768 && menuBars) { 
+        menu.classList.toggle('is-active')
+        menuLinks.classList.remove('active')
+    }
+}
+
+menuLinks.addEventListener('click', hideMobileMenu);
+menuLinks.addEventListener('click', hideMobileMenu);
